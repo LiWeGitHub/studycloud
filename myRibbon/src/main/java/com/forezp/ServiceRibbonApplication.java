8570112cb33aa399be7e63448d95deef2deb2d80
lw <hugo.liu@shangxue.la>
2018-05-28 15:55:58 +0800

    spring cloud + ribbon断路器

--- myRibbon/src/main/java/com/forezp/ServiceRibbonApplication.java
+++ myRibbon/src/main/java/com/forezp/ServiceRibbonApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -11,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableDiscoveryClient
 @RibbonClient(value = "IDSERVER")
+@EnableHystrix
 public class ServiceRibbonApplication {
 
     public static void main(String[] args) {
