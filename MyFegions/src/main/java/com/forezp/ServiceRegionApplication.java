fd91ea27f5f17c082b89f72704a34956eb1a47f5
lw <hugo.liu@shangxue.la>
2018-05-28 10:10:52 +0800

    spring cloud + fegion

---
+++ MyFegions/src/main/java/com/forezp/ServiceRegionApplication.java
@@ -0,0 +1,15 @@
+package com.forezp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class ServiceRegionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceRegionApplication.class, args);
+    }
+}
